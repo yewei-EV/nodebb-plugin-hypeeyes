bcff257860ef1ff664b6277cfe022bc38f928d5d
gengpei <gengpei@tianbaotravel.com>
2019-11-04 18:28:45 -0500

    hypeeyes dispath web uri

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,14 @@ import { UserService } from './entities/user/user.service';
 import { translateIntercept } from './common/translate';
 
 import { WordFilter } from './sensitive-words/word-filter';
+import { angularRoute } from './common/angular';
 
 export const wordFilter = new WordFilter();
 
 export async function init(params: {router: Router, app: Router}, callback: () => {}) {
+  await angularRoute(params.router);
   const router: Router = Router();
-  params.router.use('/hypeeyes/api/', router);
+  params.router.use('/hypeeyes/api', router);
   // params.app.use(translateIntercept);
   const app = await NestFactory.create(AppModule, new ExpressAdapter(router));
   await app.useGlobalInterceptors(new UserInterceptor(app.get(UserService)));
