093aec67414b4501ec05815fb7db08d6c5748463
penn <gengpei1985@gmail.com>
2019-11-26 03:05:47 -0500

    fix font awsome bug

--- src/common/angular.ts
+++ src/common/angular.ts
@@ -1,23 +1,24 @@
 import { Router } from 'express';
 import * as path from 'path';
+import * as fs from 'fs';
+import mime from 'mime-type/with-db';
 
 export async function angularRoute(router: Router) {
   router.get('/hypeeyes/web*', (req, res) => {
     const url = req.path;
-    let contentType = 'text/html';
+    let contentType: string|string[] = 'text/html';
     const contentTypeMap = {
       css: 'text/css',
       js: 'text/javascript',
       json: 'application/json',
     };
-    let file = path.resolve('../hypeeyes-web/dist/hypeeyes-web/index.html');
-    for (const postfix of Object.keys(contentTypeMap)) {
-      if (url.endsWith(postfix)) {
-        contentType = contentTypeMap[postfix];
-        file = path.resolve('../hypeeyes-web/dist/hypeeyes-web/' + url.substring(14));
-        break;
-      }
+    let file = path.resolve('../hypeeyes-web/dist/hypeeyes-web/') + + url.substring(14);
+    if (fs.existsSync(file)) {
+      contentType = mime.lookup(path.extname(file));
+    } else {
+      file = path.resolve('../hypeeyes-web/dist/hypeeyes-web/index.html');
     }
+
     res.setHeader('Content-Type', contentType);
     res.sendFile(file);
   });
