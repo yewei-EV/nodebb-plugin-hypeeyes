11c3c7daf90b951bd2c84b7bafa7d8ec642f8023
penn <gengpei1985@gmail.com>
2019-11-29 22:41:32 -0500

    fix bugs

--- src/common/angular.ts
+++ src/common/angular.ts
@@ -1,18 +1,13 @@
 import { Router } from 'express';
 import * as path from 'path';
 import * as fs from 'fs';
-import mime from 'mime-type/with-db';
+import * as mime from 'mime-types';
 
 export async function angularRoute(router: Router) {
   router.get('/hypeeyes/web*', (req, res) => {
     const url = req.path;
     let contentType: string|string[] = 'text/html';
-    const contentTypeMap = {
-      css: 'text/css',
-      js: 'text/javascript',
-      json: 'application/json',
-    };
-    let newUrl = url.substring(13);
+    const newUrl = url.substring(13);
     let file = path.resolve('../hypeeyes-web/dist/hypeeyes-web/') + newUrl;
     if (fs.existsSync(file) && newUrl.length > 1) {
       contentType = mime.lookup(path.extname(file));
