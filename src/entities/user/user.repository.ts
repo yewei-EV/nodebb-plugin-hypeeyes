a4a8ceff2e40a21cc34810e9430f5662dc158f89
penn <gengpei1985@gmail.com>
2020-02-16 13:23:51 -0500

    remove console log

--- src/entities/user/user.repository.ts
+++ src/entities/user/user.repository.ts
@@ -4,7 +4,6 @@ export class UserRepository {
   private userLib = userLib;
   async incrementReputation(uid: number, value: number): Promise<number> {
     const newReputation = await this.userLib.incrementUserFieldBy(uid, 'reputation', value);
-    console.log(newReputation);
     await db.sortedSetAdd('users:reputation', newReputation, uid);
     return newReputation;
   }
